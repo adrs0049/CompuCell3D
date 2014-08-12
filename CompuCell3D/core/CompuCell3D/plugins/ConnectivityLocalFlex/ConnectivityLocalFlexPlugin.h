0ce586d2dbe1b4001f5bb57f05a3cf47b37f11f8
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-08-12 01:54:28 -0600

    adapth Connectiveity LocalFlex

--- CompuCell3D/core/CompuCell3D/plugins/ConnectivityLocalFlex/ConnectivityLocalFlexPlugin.h
+++ CompuCell3D/core/CompuCell3D/plugins/ConnectivityLocalFlex/ConnectivityLocalFlexPlugin.h
@@ -23,74 +23,66 @@
 #ifndef CONNECTIVITYLOCALFLEXPLUGIN_H
 #define CONNECTIVITYLOCALFLEXPLUGIN_H
 
- #include <CompuCell3D/CC3D.h>
+#include <CompuCell3D/CC3D.h>
 #include "ConnectivityLocalFlexData.h"
 
-// // // #include <CompuCell3D/Potts3D/EnergyFunction.h>
-
-// // // #include <CompuCell3D/Plugin.h>
-
-// // // #include <BasicUtils/BasicClassAccessor.h>
-// // // #include <BasicUtils/BasicClassGroup.h> //had to include it to avoid problems with template instantiation
 #include "ConnectivityLocalFlexDLLSpecifier.h"
 
-
 namespace CompuCell3D {
-  
-  class CellG;
-  class Potts3D;
-  class Automaton;
-  class BoundaryStrategy;
-  class Simulator;
 
-  class CONNECTIVITYLOCALFLEX_EXPORT  ConnectivityLocalFlexPlugin : public Plugin,public EnergyFunction {
-  
-	BasicClassAccessor<ConnectivityLocalFlexData> connectivityLocalFlexDataAccessor;
-	 //Energy Function data
-    Potts3D *potts;
+class CellG;
+class Potts3D;
+class Automaton;
+class BoundaryStrategy;
+class Simulator;
 
-    // std::vector<CellG*> uniqueCells;    
-    // double penalty;
+class CONNECTIVITYLOCALFLEX_EXPORT  ConnectivityLocalFlexPlugin : public Plugin,public EnergyFunction {
 
-    // bool mediumFlag;
-    unsigned int numberOfNeighbors;
-    std::vector<int> offsetsIndex; //this vector will contain indexes of offsets in the neighborListVector so that accessing
-    //them using offsetsindex will ensure correct clockwise ordering
-    //std::vector<Point3D> n;
-    unsigned int maxNeighborIndex;
-    BoundaryStrategy * boundaryStrategy;
+     using DataAccessor_t = BasicClassAccessor<ConnectivityLocalFlexData>;
+     DataAccessor_t connectivityLocalFlexDataAccessor;
+     //Energy Function data
+     Potts3D *potts;
 
-  public:
-    ConnectivityLocalFlexPlugin();
-    virtual ~ConnectivityLocalFlexPlugin();
+     // std::vector<CellG*> uniqueCells;
+     // double penalty;
 
-	 //EnergyFunction interface
-    virtual double changeEnergy(const Point3D &pt, const CellG *newCell,
-                                const CellG *oldCell);
-	//Plugin interface
-    virtual void init(Simulator *simulator, CC3DXMLElement *_xmlData);
+     // bool mediumFlag;
+     unsigned int numberOfNeighbors;
+     std::vector<int> offsetsIndex; //this vector will contain indexes of offsets in the neighborListVector so that accessing
+     //them using offsetsindex will ensure correct clockwise ordering
+     //std::vector<Point3D> n;
+     unsigned int maxNeighborIndex;
+     BoundaryStrategy * boundaryStrategy;
 
-	BasicClassAccessor<ConnectivityLocalFlexData> * getConnectivityLocalFlexDataPtr(){return & connectivityLocalFlexDataAccessor;}
-	void setConnectivityStrength(CellG * _cell, double _connectivityStrength);
-	double getConnectivityStrength(CellG * _cell);
-	
-	//Steerable interface
-	virtual void update(CC3DXMLElement *_xmlData, bool _fullInitFlag=false);
-	virtual std::string steerableName();
-	virtual std::string toString();
+public:
+     ConnectivityLocalFlexPlugin();
+     virtual ~ConnectivityLocalFlexPlugin();
 
-	//EnergyFunction methods
-  protected:
-    /**
-     * @return The index used for ordering connectivity energies in the map.
-     */
-    void addUnique(CellG*,std::vector<CellG*> &);
-    void initializeNeighborsOffsets();
-    void orderNeighborsClockwise(Point3D & _midPoint, const std::vector<Point3D> & _offsets, std::vector<int> & _offsetsIndex);
+     //EnergyFunction interface
+     virtual double changeEnergy ( const Point3D &pt, const CellG *newCell,
+                                   const CellG *oldCell );
+     //Plugin interface
+     virtual void init ( Simulator *simulator, CC3DXMLElement *_xmlData );
 
+     DataAccessor_t * getConnectivityLocalFlexDataPtr() {
+          return & connectivityLocalFlexDataAccessor;
+     }
+     void setConnectivityStrength ( CellG * _cell, double _connectivityStrength );
+     double getConnectivityStrength ( CellG * _cell );
 
+     //Steerable interface
+     virtual void update ( CC3DXMLElement *_xmlData, bool _fullInitFlag=false );
+     virtual std::string steerableName();
+     virtual std::string toString();
 
-
-  };
+     //EnergyFunction methods
+protected:
+     /**
+      * @return The index used for ordering connectivity energies in the map.
+      */
+     void addUnique ( CellG*,std::vector<CellG*> & );
+     void initializeNeighborsOffsets();
+     void orderNeighborsClockwise ( Point3D & _midPoint, const std::vector<Point3D> & _offsets, std::vector<int> & _offsetsIndex );
+};
 };
 #endif
