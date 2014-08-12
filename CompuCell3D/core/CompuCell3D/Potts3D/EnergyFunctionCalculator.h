fe60991661831bcadd7d15fddee3544197a942f5
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-08-12 17:12:29 -0600

    reformat EnergyFunctionCalculator

--- CompuCell3D/core/CompuCell3D/Potts3D/EnergyFunctionCalculator.h
+++ CompuCell3D/core/CompuCell3D/Potts3D/EnergyFunctionCalculator.h
@@ -4,10 +4,10 @@
 #include <vector>
 #include <map>
 
-
 class CC3DXMLElement;
 
-namespace CompuCell3D{
+namespace CompuCell3D
+{
 
 class EnergyFunction;
 class CellG;
@@ -16,39 +16,44 @@ class Potts3D;
 class Simulator;
 class ParseData;
 
-class EnergyFunctionCalculator{
+class EnergyFunctionCalculator
+{
 
-   public:
-      EnergyFunctionCalculator();
-      virtual ~EnergyFunctionCalculator();
+public:
+    EnergyFunctionCalculator();
+    virtual ~EnergyFunctionCalculator();
 
-		virtual void init(CC3DXMLElement *_xmlData){}
-      virtual void registerEnergyFunction(EnergyFunction *_function);
-      virtual void registerEnergyFunctionWithName(EnergyFunction *_function,std::string _functionName);
-      virtual void unregisterEnergyFunction(std::string _functionName);
-      virtual void configureEnergyCalculator(std::vector<std::string> &_configVector){}
-      virtual double changeEnergy(Point3D &pt, const CellG *newCell,
-				const CellG *oldCell,const unsigned int _flipAttempt);
-      void setPotts(Potts3D * _potts){potts=_potts;}
-      void setSimulator(Simulator * _sim){sim=_sim ;}
+    virtual void init ( CC3DXMLElement *_xmlData ) {}
+    virtual void registerEnergyFunction ( EnergyFunction *_function );
+    virtual void registerEnergyFunctionWithName ( EnergyFunction *_function,std::string _functionName );
+    virtual void unregisterEnergyFunction ( std::string _functionName );
+    virtual void configureEnergyCalculator ( std::vector<std::string> &_configVector ) {}
+    virtual double changeEnergy ( Point3D &pt, const CellG *newCell,
+                                  const CellG *oldCell,const unsigned int _flipAttempt );
+    void setPotts ( Potts3D * _potts )
+    {
+        potts=_potts;
+    }
+    void setSimulator ( Simulator * _sim )
+    {
+        sim=_sim ;
+    }
 
+    virtual void setLastFlipAccepted ( bool _accept )
+    {
+        lastFlipAccepted=_accept;
+    }
 
+protected:
+    std::vector<EnergyFunction *> energyFunctions;
+    std::vector<std::string> energyFunctionsNameVec;
 
-      virtual void setLastFlipAccepted(bool _accept){lastFlipAccepted=_accept;}
-   protected:
-      std::vector<EnergyFunction *> energyFunctions;
-      std::vector<std::string> energyFunctionsNameVec;
+    std::map<std::string,EnergyFunction *> nameToEnergyFuctionMap;
+    Potts3D *potts;
+    Simulator *sim;
 
-      std::map<std::string,EnergyFunction *> nameToEnergyFuctionMap;
-      Potts3D *potts;
-      Simulator *sim;
-
-      bool lastFlipAccepted;
-
+    bool lastFlipAccepted;
 };
 
-};
-
-
+} // end namespace
 #endif
-
