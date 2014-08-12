138b4ed2ddda935ef42afa160614bb8f16a94d41
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-08-12 16:58:36 -0600

    Reformat FluctAmplFunc + add Destructor

--- CompuCell3D/core/CompuCell3D/Potts3D/FluctuationAmplitudeFunction.h
+++ CompuCell3D/core/CompuCell3D/Potts3D/FluctuationAmplitudeFunction.h
@@ -1,40 +1,38 @@
 
 #ifndef FLUCTUATIONAMPLITUDEFUNCTION_H
 #define FLUCTUATIONAMPLITUDEFUNCTION_H
-#include <vector>
 
-namespace CompuCell3D {
+namespace CompuCell3D
+{
+class Potts3D;
+class CellG;
 
+class FluctuationAmplitudeFunction
+{
 
-	class Potts3D;
-	class CellG;
+public:
+    FluctuationAmplitudeFunction ( const Potts3D * _potts ) :
+        potts ( _potts )
+    {}
 
-	class FluctuationAmplitudeFunction {
+    /**
+    * Calculates the fluctuationAmplitude based on source/destination cells fluctuation amplitudes
+    * Fluctuation Amplitude is "more biological" code for temperature parameter used in classical POtts
+    *
+    *
+    * @param _oldCell - destination cell.
+    * @param _newCell - source cell.
+    *
+    * @return fluctuationAmplitude - in case fluctAmpl cell attribute is set to negative number we return global temperatue as a fluctuation Amplitude.
+    */
 
-	public:
-		FluctuationAmplitudeFunction(const Potts3D * _potts):
-		  potts(_potts)
-		{}
+    virtual double fluctuationAmplitude ( const CellG * newCell, const CellG * oldCell ) = 0;
 
-		/** 
-		* Calculates the fluctuationAmplitude based on source/destination cells fluctuation amplitudes
-		* Fluctuation Amplitude is "more biological" code for temperature parameter used in classical POtts 
-		* 
-		* 
-		* @param _oldCell - destination cell.
-		* @param _newCell - source cell.
-		* 
-		* @return fluctuationAmplitude - in case fluctAmpl cell attribute is set to negative number we return global temperatue as a fluctuation Amplitude.
-		*/
-
-		virtual double fluctuationAmplitude(const CellG * newCell, const CellG * oldCell) = 0;
-
-	protected:
-		const Potts3D *potts;
-		
-
-	};
-
+	~FluctuationAmplitudeFunction() {}
+	
+protected:
+    const Potts3D *potts;
+};
 
 };
 #endif
--- CompuCell3D/core/CompuCell3D/Potts3D/StandardFluctuationAmplitudeFunctions.h
+++ CompuCell3D/core/CompuCell3D/Potts3D/StandardFluctuationAmplitudeFunctions.h
@@ -25,72 +25,81 @@
 
 #include "FluctuationAmplitudeFunction.h"
 
-namespace CompuCell3D {
+namespace CompuCell3D
+{
 
 
-	class MinFluctuationAmplitudeFunction:public FluctuationAmplitudeFunction{
+class MinFluctuationAmplitudeFunction:public FluctuationAmplitudeFunction
+{
 
-	public:
-		MinFluctuationAmplitudeFunction(const Potts3D * _potts);
+public:
+    MinFluctuationAmplitudeFunction ( const Potts3D * _potts );
 
-		/** 
-		* Calculates the fluctuationAmplitude based on source/destination cells fluctuation amplitudes
-		* Fluctuation Amplitude is "more biological" code for temperature parameter used in classical POtts 
-		* 
-		* 
-		* @param _oldCell - destination cell.
-		* @param _newCell - source cell.
-		* 
-		* @return fluctuationAmplitude - in case fluctAmpl cell attribute is set to negative number we return global temperatue as a fluctuation Amplitude.
-		*/
+    /**
+    * Calculates the fluctuationAmplitude based on source/destination cells fluctuation amplitudes
+    * Fluctuation Amplitude is "more biological" code for temperature parameter used in classical POtts
+    *
+    *
+    * @param _oldCell - destination cell.
+    * @param _newCell - source cell.
+    *
+    * @return fluctuationAmplitude - in case fluctAmpl cell attribute is set to negative number we return global temperatue as a fluctuation Amplitude.
+    */
 
-                virtual double
-                fluctuationAmplitude(const CellG *newCell,
-                                     const CellG *oldCell) override;
-        };
+    virtual double
+    fluctuationAmplitude ( const CellG *newCell,
+                           const CellG *oldCell ) override;
 
-	class MaxFluctuationAmplitudeFunction:public FluctuationAmplitudeFunction{
-
-	public:
-		MaxFluctuationAmplitudeFunction(const Potts3D * _potts);
-
-		/** 
-		* Calculates the fluctuationAmplitude based on source/destination cells fluctuation amplitudes
-		* Fluctuation Amplitude is "more biological" code for temperature parameter used in classical POtts 
-		* 
-		* 
-		* @param _oldCell - destination cell.
-		* @param _newCell - source cell.
-		* 
-		* @return fluctuationAmplitude - in case fluctAmpl cell attribute is set to negative number we return global temperatue as a fluctuation Amplitude.
-		*/
-
-                virtual double
-                fluctuationAmplitude(const CellG *newCell,
-                                     const CellG *oldCell) override;
-        };
-
-	class ArithmeticAverageFluctuationAmplitudeFunction:public FluctuationAmplitudeFunction{
-
-	public:
-		ArithmeticAverageFluctuationAmplitudeFunction(const Potts3D * _potts);
-
-		/** 
-		* Calculates the fluctuationAmplitude based on source/destination cells fluctuation amplitudes
-		* Fluctuation Amplitude is "more biological" code for temperature parameter used in classical POtts 
-		* 
-		* 
-		* @param _oldCell - destination cell.
-		* @param _newCell - source cell.
-		* 
-		* @return fluctuationAmplitude - in case fluctAmpl cell attribute is set to negative number we return global temperatue as a fluctuation Amplitude.
-		*/
-
-                virtual double
-                fluctuationAmplitude(const CellG *newCell,
-                                     const CellG *oldCell) override;
-        };
-
-
+	~MinFluctuationAmplitudeFunction() {}
 };
+
+class MaxFluctuationAmplitudeFunction:public FluctuationAmplitudeFunction
+{
+
+public:
+    MaxFluctuationAmplitudeFunction ( const Potts3D * _potts );
+
+    /**
+    * Calculates the fluctuationAmplitude based on source/destination cells fluctuation amplitudes
+    * Fluctuation Amplitude is "more biological" code for temperature parameter used in classical POtts
+    *
+    *
+    * @param _oldCell - destination cell.
+    * @param _newCell - source cell.
+    *
+    * @return fluctuationAmplitude - in case fluctAmpl cell attribute is set to negative number we return global temperatue as a fluctuation Amplitude.
+    */
+
+    virtual double
+    fluctuationAmplitude ( const CellG *newCell,
+                           const CellG *oldCell ) override;
+						   
+	~MaxFluctuationAmplitudeFunction() {}
+};
+
+class ArithmeticAverageFluctuationAmplitudeFunction:public FluctuationAmplitudeFunction
+{
+
+public:
+    ArithmeticAverageFluctuationAmplitudeFunction ( const Potts3D * _potts );
+
+    /**
+    * Calculates the fluctuationAmplitude based on source/destination cells fluctuation amplitudes
+    * Fluctuation Amplitude is "more biological" code for temperature parameter used in classical POtts
+    *
+    *
+    * @param _oldCell - destination cell.
+    * @param _newCell - source cell.
+    *
+    * @return fluctuationAmplitude - in case fluctAmpl cell attribute is set to negative number we return global temperatue as a fluctuation Amplitude.
+    */
+
+    virtual double
+    fluctuationAmplitude ( const CellG *newCell,
+                           const CellG *oldCell ) override;
+						   
+	~ArithmeticAverageFluctuationAmplitudeFunction() {}
+};
+
+} // end namespace
 #endif
