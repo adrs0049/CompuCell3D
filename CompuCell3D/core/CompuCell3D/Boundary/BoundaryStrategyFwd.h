cd93708ca005f0f6938f81a8932e6d0da59ce0e7
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-08-12 21:36:14 -0600

    fwd def of BoundaryStrategy

---
+++ CompuCell3D/core/CompuCell3D/Boundary/BoundaryStrategyFwd.h
@@ -0,0 +1,13 @@
+#ifndef BOUNDARY_STRATEGY_FWD_H
+#define BOUNDARY_STRATEGY_FWD_H
+
+#include <memory>
+
+namespace CompuCell3D {
+
+class BoundaryStrategy;
+using BoundaryStrategyPtr = std::shared_ptr<BoundaryStrategy>;
+	
+} // end namespace
+
+#endif
