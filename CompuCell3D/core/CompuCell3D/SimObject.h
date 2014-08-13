e28dbdcd74f0c05eeec5f63c9571567d47af6356
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-08-12 23:07:35 -0600

    reformat SimObject

--- CompuCell3D/core/CompuCell3D/SimObject.h
+++ CompuCell3D/core/CompuCell3D/SimObject.h
@@ -23,34 +23,44 @@
 #ifndef SIMOBJECT_H
 #define SIMOBJECT_H
 
-
 #include <string>
 #include <CompuCell3D/SteerableObject.h>
 
 class CC3DXMLElement;
 
-namespace CompuCell3D {
-  class Simulator;
-  class ParseData;
-  class CC3DEvent;
+namespace CompuCell3D
+{
+class Simulator;
+class ParseData;
+class CC3DEvent;
 
-  class SimObject : public virtual SteerableObject {
-  protected:
+class SimObject : public virtual SteerableObject
+{
+protected:
     Simulator * simulator;
     ParseData * pd;
-  public:
-    SimObject() : simulator(nullptr), pd(nullptr) {}
+public:
+    SimObject() : simulator ( nullptr ), pd ( nullptr ) {}
     virtual ~SimObject() {}
 
-    virtual void init(Simulator *simulator,
-                      CC3DXMLElement *_xmlData = nullptr) {
-      this->simulator = simulator;
+    virtual void init ( Simulator *simulator,
+                        CC3DXMLElement *_xmlData = nullptr )
+    {
+        this->simulator = simulator;
     }
-    virtual void extraInit(Simulator *simulator){this->simulator = simulator;}
-    virtual std::string toString(){return "SimObject";}
-    virtual ParseData * getParseData(){return pd;}
-    virtual void handleEvent(CC3DEvent & _event){}
-
-  };
-}
+    virtual void extraInit ( Simulator *simulator )
+    {
+        this->simulator = simulator;
+    }
+    virtual std::string toString()
+    {
+        return "SimObject";
+    }
+    virtual ParseData * getParseData()
+    {
+        return pd;
+    }
+    virtual void handleEvent ( CC3DEvent & _event ) {}
+};
+} // end namespace
 #endif
