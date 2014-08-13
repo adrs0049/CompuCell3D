576c899ae6fef2e0acff2420bc1098c064311fac
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-08-12 19:10:17 -0600

    reformat boundarytypedefs

--- CompuCell3D/core/CompuCell3D/Boundary/BoundaryTypeDefinitions.h
+++ CompuCell3D/core/CompuCell3D/Boundary/BoundaryTypeDefinitions.h
@@ -23,26 +23,24 @@
 #ifndef BOUNDARYTYPEDEFINITIONS_H
 #define BOUNDARYTYPEDEFINITIONS_H
 
-#include <string>
+namespace CompuCell3D
+{
 
-//using namespace std;
+enum LatticeType {SQUARE_LATTICE=1,HEXAGONAL_LATTICE=2};
 
-namespace CompuCell3D {
-
-   enum LatticeType {SQUARE_LATTICE=1,HEXAGONAL_LATTICE=2};    
-
-   class LatticeMultiplicativeFactors{
-      public:
-         LatticeMultiplicativeFactors(double _volumeMF=1.0, double _surfaceMF=1.0, double _lengthMF=1.0):
-         volumeMF(_volumeMF),
-         surfaceMF(_surfaceMF),
-         lengthMF(_lengthMF)
-         {}
-         double volumeMF;
-         double surfaceMF;
-         double lengthMF;
-   };
-
+class LatticeMultiplicativeFactors
+{
+public:
+    LatticeMultiplicativeFactors ( double _volumeMF=1.0, double _surfaceMF=1.0, double _lengthMF=1.0 ) :
+        volumeMF ( _volumeMF ),
+        surfaceMF ( _surfaceMF ),
+        lengthMF ( _lengthMF )
+    {}
+    double volumeMF;
+    double surfaceMF;
+    double lengthMF;
 };
+
+} // end namespace
 
 #endif
