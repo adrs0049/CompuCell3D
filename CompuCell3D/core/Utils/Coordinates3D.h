4f1cb743831d61216c189f37c3897188a6779cba
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-08-12 19:01:51 -0600

    reduce compile time dep + reformat

--- CompuCell3D/core/Utils/Coordinates3D.h
+++ CompuCell3D/core/Utils/Coordinates3D.h
@@ -1,126 +1,153 @@
 #ifndef COORDINATES3D_H
 #define COORDINATES3D_H
-#include <iostream>
+#include <ostream>
 #include <cmath>
 
 ///T must provide copy constructor and operator=
 template <typename T>
-class Coordinates3D{
+class Coordinates3D
+{
 
-   public:
-      Coordinates3D():
-                     x(T(0)),
-                     y(T(0)),
-                     z(T(0))
-                     
-      {}
-      
-      Coordinates3D(const T & _x,const T & _y,const T & _z){
-         x=_x;
-         y=_y;
-         z=_z;
-      }
+public:
+    Coordinates3D() :
+        x ( T ( 0 ) ),
+        y ( T ( 0 ) ),
+        z ( T ( 0 ) )
 
-      T  X() const {return x;}
-      T  Y() const {return y;}
-      T  Z() const {return z;}
+    {}
 
-      T&  XRef(){return x;}
-      T&  YRef(){return y;}
-      T&  ZRef(){return z;}
+    Coordinates3D ( const T & _x,const T & _y,const T & _z )
+    {
+        x=_x;
+        y=_y;
+        z=_z;
+    }
 
-      Coordinates3D<T> & operator-=(const Coordinates3D<T> &a){
-         x-=a.X();
-         y-=a.Y();
-         z-=a.Z();
-         return *this;
-      }
-      Coordinates3D<T> & operator+=(const Coordinates3D<T> &a){
-         x+=a.X();
-         y+=a.Y();
-         z+=a.Z();
-         return *this;
-      }
+    T  X() const
+    {
+        return x;
+    }
+    T  Y() const
+    {
+        return y;
+    }
+    T  Z() const
+    {
+        return z;
+    }
 
-      Coordinates3D<T> & operator=(const Coordinates3D<T> &a){
-         x=a.X();
-         y=a.Y();
-         z=a.Z();
-         return *this;
-      }
-      
-	  Coordinates3D<T> & operator*=(T val)
-	  {
-		 x*=val;
-		 y*=val;
-		 z*=val;
-		 return *this;
-	  }
-   
-      T x;
-      T y;
-      T z;
+    T&  XRef()
+    {
+        return x;
+    }
+    T&  YRef()
+    {
+        return y;
+    }
+    T&  ZRef()
+    {
+        return z;
+    }
+
+    Coordinates3D<T> & operator-= ( const Coordinates3D<T> &a )
+    {
+        x-=a.X();
+        y-=a.Y();
+        z-=a.Z();
+        return *this;
+    }
+    Coordinates3D<T> & operator+= ( const Coordinates3D<T> &a )
+    {
+        x+=a.X();
+        y+=a.Y();
+        z+=a.Z();
+        return *this;
+    }
+
+    Coordinates3D<T> & operator= ( const Coordinates3D<T> &a )
+    {
+        x=a.X();
+        y=a.Y();
+        z=a.Z();
+        return *this;
+    }
+
+    Coordinates3D<T> & operator*= ( T val )
+    {
+        x*=val;
+        y*=val;
+        z*=val;
+        return *this;
+    }
+
+    T x;
+    T y;
+    T z;
 
 };
 
-
 template<typename T>
-Coordinates3D<T> crossProduct(const Coordinates3D<T> &a,const Coordinates3D<T> &b){
-   return Coordinates3D<T>(a.y*b.z-a.z*b.y , a.z*b.x-a.x*b.z , a.x*b.y-a.y*b.x);
+Coordinates3D<T> crossProduct ( const Coordinates3D<T> &a,const Coordinates3D<T> &b )
+{
+    return Coordinates3D<T> ( a.y*b.z-a.z*b.y , a.z*b.x-a.x*b.z , a.x*b.y-a.y*b.x );
 }
 
 template<typename T>
-Coordinates3D<T> operator+(const Coordinates3D<T> &a,const Coordinates3D<T> &b){
-   return Coordinates3D<T>(a.X()+b.X(),a.Y()+b.Y(),a.Z()+b.Z());
+Coordinates3D<T> operator+ ( const Coordinates3D<T> &a,const Coordinates3D<T> &b )
+{
+    return Coordinates3D<T> ( a.X() +b.X(),a.Y() +b.Y(),a.Z() +b.Z() );
 }
 
 template<typename T>
-Coordinates3D<T> operator-(const Coordinates3D<T> &a,const Coordinates3D<T> &b){
-   return Coordinates3D<T>(a.X()-b.X(),a.Y()-b.Y(),a.Z()-b.Z());
+Coordinates3D<T> operator- ( const Coordinates3D<T> &a,const Coordinates3D<T> &b )
+{
+    return Coordinates3D<T> ( a.X()-b.X(),a.Y()-b.Y(),a.Z()-b.Z() );
 }
 
 template<typename T>
-Coordinates3D<T> operator*(const Coordinates3D<T> &a, T val){
-	Coordinates3D<T> r (a);
-	r*=val;
-	return r;
+Coordinates3D<T> operator* ( const Coordinates3D<T> &a, T val )
+{
+    Coordinates3D<T> r ( a );
+    r*=val;
+    return r;
 }
 
 template<typename T>
-Coordinates3D<T> operator*(T val, const Coordinates3D<T> &a){
-	return Coordinates3D<T>(a*val);
+Coordinates3D<T> operator* ( T val, const Coordinates3D<T> &a )
+{
+    return Coordinates3D<T> ( a*val );
 }
 
 template<typename T> //scalar product
-T operator*(const Coordinates3D<T> &a,const Coordinates3D<T> &b){
-   return a.x*b.x+a.y*b.y+a.z*b.z;
+T operator* ( const Coordinates3D<T> &a,const Coordinates3D<T> &b )
+{
+    return a.x*b.x+a.y*b.y+a.z*b.z;
 }
 
 template<typename T>
-T norm2(const Coordinates3D<T> &a){
-	return std::sqrt(a * a);
-}
-   
-template<typename T>
-std::ostream & operator<<(std::ostream &_out,Coordinates3D<T> & _coordinates){
-
-      _out<<_coordinates.X()<<" , "<<_coordinates.Y()<<" , "<<_coordinates.Z();
-	  return _out;
-
+T norm2 ( const Coordinates3D<T> &a )
+{
+    return std::sqrt ( a * a );
 }
 
 template<typename T>
-Coordinates3D<T> operator<(const Coordinates3D<T> &a,const Coordinates3D<T> &b){
-   return a.X()<b.X() || (!(a.X()<b.X()) && a.Y()< b.Y()  ) || (!(a.X()<b.X()) && !(a.Y()< b.Y()) &&  a.Z()< b.Z());
+std::ostream & operator<< ( std::ostream &_out,Coordinates3D<T> & _coordinates )
+{
+    _out<<_coordinates.X() <<" , "<<_coordinates.Y() <<" , "<<_coordinates.Z();
+    return _out;
 }
-
 
 template<typename T>
-std::ostream & operator<<(std::ostream & _out, const Coordinates3D<T> &_a){
-   _out<<"("<<_a.X()<<","<<_a.Y()<<","<<_a.Z()<<")";
-   return _out;
+Coordinates3D<T> operator< ( const Coordinates3D<T> &a,const Coordinates3D<T> &b )
+{
+    return a.X() <b.X() || ( ! ( a.X() <b.X() ) && a.Y() < b.Y() ) || ( ! ( a.X() <b.X() ) && ! ( a.Y() < b.Y() ) &&  a.Z() < b.Z() );
 }
 
-
+template<typename T>
+std::ostream & operator<< ( std::ostream & _out, const Coordinates3D<T> &_a )
+{
+    _out<<"("<<_a.X() <<","<<_a.Y() <<","<<_a.Z() <<")";
+    return _out;
+}
 
 #endif
