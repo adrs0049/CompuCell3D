1ca34387fe4a32c31cd3588c4595bb1acbeb295d
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-08-12 18:55:56 -0600

    update StringUtils

--- CompuCell3D/core/PublicUtilities/StringUtils.h
+++ CompuCell3D/core/PublicUtilities/StringUtils.h
@@ -2,43 +2,48 @@
 #define STRINGUTILS_H
 #include <vector>
 #include <string>
-#include <algorithm>
 #include <locale>
-#include <cctype>
-#include <functional>
 
-void parseStringIntoList(std::string &str,std::vector<std::string> &strVec,std::string separator);
+void parseStringIntoList ( std::string &str,std::vector<std::string> &strVec,std::string separator );
 
-class isWhiteSpaceFunctor: public std::unary_function<char,bool>{
-   private:
-   std::locale loc;
-   
-   public:
-      bool operator()(char c)const{
-         return isspace(c,loc);
+class isWhiteSpaceFunctor: public std::unary_function<char,bool>
+{
+private:
+    std::locale loc;
 
-      }
-
+public:
+    bool operator() ( char c ) const
+    {
+        return isspace ( c,loc );
+    }
 };
-
 
 struct ToLower
 {
-   ToLower(std::locale const& l) : loc(l) {;}
-   char operator() (char c) const  { return std::tolower(c,loc); }
-   private:
-      std::locale const& loc;
+    ToLower ( std::locale const& l ) : loc ( l )
+    {}
+    
+    char operator() ( char c ) const
+    {
+        return std::tolower ( c,loc );
+    }
+private:
+    std::locale const& loc;
 };
-
 
 struct ToUpper
 {
-      ToUpper(std::locale const& l) : loc(l) {;}
-      char operator() (char c) const  { return std::toupper(c,loc); }
+    ToUpper ( std::locale const& l ) : loc ( l )
+    {}
+    
+    char operator() ( char c ) const
+    {
+        return std::toupper ( c,loc );
+    }
 private:
-      std::locale const& loc;
+    std::locale const& loc;
 };
 
-void changeToLower(std::string & str);
-void changeToUpper(std::string & str);
+void changeToLower ( std::string & str );
+void changeToUpper ( std::string & str );
 #endif
