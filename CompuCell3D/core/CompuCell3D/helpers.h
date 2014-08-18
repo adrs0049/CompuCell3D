869c1fa5bb6b7f088ceecc7dfdb343ea3b389d0a
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-08-17 18:25:05 -0600

    inline helper function

--- CompuCell3D/core/CompuCell3D/helpers.h
+++ CompuCell3D/core/CompuCell3D/helpers.h
@@ -9,21 +9,21 @@ class Simulator;
 class Potts3D;
 	
 template <typename Plugin>
-std::shared_ptr<Plugin>
+inline std::shared_ptr<Plugin>
 get_plugin(const std::string pluginName, bool * flag = nullptr)
 {
 	return std::static_pointer_cast<Plugin>(Simulator::pluginManager.get(pluginName, flag));
 }
 	
 template <typename Steppable>
-std::shared_ptr<Steppable>
+inline std::shared_ptr<Steppable>
 get_steppable(const std::string steppableName, bool * flag = nullptr)
 {
 	return std::static_pointer_cast<Steppable>(Simulator::steppableManager.get(steppableName, flag));
 }
 
 template <typename Accessor>
-void
+inline void
 registerClassOnCell(Potts3D * potts, Accessor& TheAccessor)
 {
 	potts->getCellFactoryGroupPtr()->registerClass(std::make_shared<Accessor>(TheAccessor));
