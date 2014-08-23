00f1a90bf9e264db7e8d6dce7ada546a33fd116a
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-08-23 01:16:28 -0600

    reformat PeriodicBcCond

--- CompuCell3D/core/CompuCell3D/Boundary/PeriodicBoundary.cpp
+++ CompuCell3D/core/CompuCell3D/Boundary/PeriodicBoundary.cpp
@@ -27,30 +27,33 @@
 using namespace std;
 using namespace CompuCell3D;
 /*
- * Apply PeriodicBoundary to the given coordinate. 
+ * Apply PeriodicBoundary to the given coordinate.
  * If the coordinate lies outside the max value take a mod and return it/
  * If the coordinate is negative, take a mod and subtract that value
- * from the max value and return it 
+ * from the max value and return it
  *
  * @param coordinate  int
  * @param max_value int
- * 
+ *
  * @return bool. If the condition was applied successfully.
- */ 
-  bool PeriodicBoundary::applyCondition(int& coordinate, const int& max_value){
-  
-      short val;
-      
-      if(coordinate < 0 ) {
-          val = std::abs((float)(coordinate % max_value));
-          coordinate = max_value - val;
-          return true;
-     } else if (coordinate >= max_value) {
-          coordinate = coordinate % max_value;
-          return true;
-     }
-      return false;
-  }
+ */
+bool PeriodicBoundary::applyCondition ( int& coordinate, const int& max_value )
+{
+    short val;
+
+    if ( coordinate < 0 )
+    {
+        val = std::abs ( ( float ) ( coordinate % max_value ) );
+        coordinate = max_value - val;
+        return true;
+    }
+    else if ( coordinate >= max_value )
+    {
+        coordinate = coordinate % max_value;
+        return true;
+    }
+    return false;
+}
 
 PeriodicBoundary::~PeriodicBoundary()
 {}
